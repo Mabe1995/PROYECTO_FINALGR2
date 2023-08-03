cfaf8055e7d564ab0d508d66ba20827104f1fec9
MABE <maberosero@hotmail.com>
2023-08-02 22:39:21 -0500

    creamos la tabla por actividad y canton

--- Scripts/moduloR_grupo2.R
+++ Scripts/moduloR_grupo2.R
@@ -14,10 +14,48 @@ balance_2014<-tibble(balance_2014)
 str(balance_2014)
 
 #Crear la base
-balance_2014 %>%
-  rename(actividad_eco= ciiu4_nivel1,subactividad=ciiu4_nivel6) %>% 
-  left_join(ciiu,by=c("actividad_eco"="CODIGO")) %>% view()
-  
-  mutate(liquidez_corriente=v345/v539     )
+balance<-balance_2014 %>%
+  rename(CODIGO= ciiu4_nivel1) %>% 
+  mutate(liquidez_corriente=v345/v539,endeu_activo=v599/v499,
+         ende_patrimo=v599/v698,ende_activo_fijo=v698/v498, 
+         apalancamiento=v499/v698) %>% 
   select(nombre_cia, situacion, tipo, pais, provincia, canton, ciudad,
-         actividad_eco,subactividad)
+         CODIGO,ciiu4_nivel6,trab_direc,trab_admin,
+         liquidez_corriente,endeu_activo,ende_patrimo,
+         ende_activo_fijo,apalancamiento) %>% 
+  merge(ciiu, by=("CODIGO")) %>% rename(actividad=DESCRIPCION)%>%
+  select(!c(CODIGO,NIVEL)) %>%  rename(CODIGO=ciiu4_nivel6) %>% 
+  merge(ciiu, by=("CODIGO")) %>% rename(subactividad=DESCRIPCION)%>%
+  select(!c(CODIGO,NIVEL)) %>% 
+  mutate(actividad=as.factor(actividad),
+         subactividad=as.factor(subactividad),
+         situacion=as.factor(situacion),
+         tipo=as.factor(tipo)) %>% 
+  view()
+
+##depurar la base
+cantidad_de_NA <- sum(is.na(balance))
+
+## total de empresas por actividad Y  canton
+balance %>% group_by(actividad,canton) %>% summarise(total_empresas=n()) %>% 
+  view("base2")
+
+## total empresas por canton 
+balance %>% group_by(canton) %>% summarise(total_empresas=n()) %>% 
+  view("base2")
+
+
+levels(as.factor(balance$situacion))
+levels(as.factor(balance$provincia))
+## Gráficamente muestra el comparativo de los indicadores financieros de liquidez y
+## solvencia por Status y provincia.
+attach(balance)
+
+azuay<-balance %>% filter(provincia=="AZUAY")
+
+ggplot(balance, aes(y=liquidez_corriente, fill = situacion)) +
+  geom_histogram() +   geom_density()+
+  labs(title = "Comparativo de Liquidez por Provincia y Status",
+       x = "Provincia", y = "Valor") +
+  facet_wrap(~provincia)+
+  theme(legend.position = "none")
