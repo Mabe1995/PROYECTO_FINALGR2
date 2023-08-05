570985de1a48b28d2c9bb9f28aa8578e6ba3ed1c
MABE <maberosero@hotmail.com>
2023-08-04 21:28:56 -0500

    se realizo los graficos comparativos y se respondio a las preguntas

--- Scripts/moduloR_grupo2.R
+++ Scripts/moduloR_grupo2.R
@@ -4,6 +4,7 @@ library(magrittr)
 library(tidyverse)
 library(tibble)
 library(tidyr)
+library(ggplot2)
 
 #Cargar bases
 balance_2014<-read.xlsx("Data/balances_2014.xlsx")
@@ -14,27 +15,26 @@ balance_2014<-tibble(balance_2014)
 str(balance_2014)
 
 #Crear la base
-balance<-balance_2014 %>%
-  rename(CODIGO= ciiu4_nivel1) %>% 
+
+balance<-
+  balance_2014 %>%
+  inner_join(ciiu, by=c("ciiu4_nivel1"="CODIGO")) %>% 
+  inner_join(ciiu, by=c("ciiu4_nivel6"="CODIGO"))%>% 
   mutate(liquidez_corriente=v345/v539,endeu_activo=v599/v499,
          ende_patrimo=v599/v698,ende_activo_fijo=v698/v498, 
          apalancamiento=v499/v698) %>% 
-  select(nombre_cia, situacion, tipo, pais, provincia, canton, ciudad,
-         CODIGO,ciiu4_nivel6,trab_direc,trab_admin,
+  rename(actividad=DESCRIPCION.x,
+         subactividad=DESCRIPCION.y) %>% 
+  select(nombre_cia, situacion, tipo, pais, provincia, canton, ciudad,fecha_const,
+         tamanio, actividad ,subactividad,trab_direc,trab_admin,
          liquidez_corriente,endeu_activo,ende_patrimo,
          ende_activo_fijo,apalancamiento) %>% 
-  merge(ciiu, by=("CODIGO")) %>% rename(actividad=DESCRIPCION)%>%
-  select(!c(CODIGO,NIVEL)) %>%  rename(CODIGO=ciiu4_nivel6) %>% 
-  merge(ciiu, by=("CODIGO")) %>% rename(subactividad=DESCRIPCION)%>%
-  select(!c(CODIGO,NIVEL)) %>% 
   mutate(actividad=as.factor(actividad),
          subactividad=as.factor(subactividad),
          situacion=as.factor(situacion),
-         tipo=as.factor(tipo)) %>% 
+         tipo=as.factor(tipo)) %>%
   view()
 
-##depurar la base
-cantidad_de_NA <- sum(is.na(balance))
 
 ## total de empresas por actividad Y  canton
 balance %>% group_by(actividad,canton) %>% summarise(total_empresas=n()) %>% 
@@ -45,17 +45,150 @@ balance %>% group_by(canton) %>% summarise(total_empresas=n()) %>%
   view("base2")
 
 
+##depurar la base
+cantidad_de_NA <- sum(is.na(balance))
+balance<-balance[is.finite(balance$liquidez_corriente), ] %>% view()
+balance<-balance[is.finite(balance$apalancamiento), ] %>% view()
+variables<-c("liquidez_corriente","apalancamiento","endeu_activo","ende_patrimo",
+             "ende_activo_fijo")
+finitos <- function(data, columna) {
+  data_filtrado <- data[is.finite(data[[columna]]), ]
+  return(data_filtrado)
+}
+
+balance<-finitos(balance,"endeu_activo") 
+balance<-finitos(balance,"ende_patrimo") 
+balance<-finitos(balance,"ende_activo_fijo") 
+colSums(is.na(balance))
+base1 <- na.omit(balance)
+colSums(is.na(base1))
+
 levels(as.factor(balance$situacion))
 levels(as.factor(balance$provincia))
+
 ## Gráficamente muestra el comparativo de los indicadores financieros de liquidez y
-## solvencia por Status y provincia.
+## solvencia por situacion y provincia.
 attach(balance)
 
-azuay<-balance %>% filter(provincia=="AZUAY")
+activas<-base1 %>% filter(situacion=="ACTIVA")
+impute_outliers <- function(x, removeNA = TRUE){
+  quantiles <- quantile(x, c(0.05, 0.95), na.rm = removeNA)
+  x[x<quantiles[1]] <- mean(x, na.rm = removeNA)
+  x[x>quantiles[2]] <- median(x, na.rm = removeNA)
+  x
+}
+activas$liquidez_corriente<-impute_outliers(activas$liquidez_corriente)
 
-ggplot(balance, aes(y=liquidez_corriente, fill = situacion)) +
-  geom_histogram() +   geom_density()+
-  labs(title = "Comparativo de Liquidez por Provincia y Status",
-       x = "Provincia", y = "Valor") +
+plot(activas$liquidez_corriente)
+ggplot(activas, aes(x=fecha_const, y=liquidez_corriente, color = provincia, group=provincia))   +
+  geom_line() +
+  geom_hline(data = activas %>% group_by(provincia) %>% summarise(ymin = quantile(liquidez_corriente, 0.25), ymed = median(liquidez_corriente), ymax = quantile(liquidez_corriente, 0.75)),
+             aes(yintercept = ymin), linetype = "dashed", color = "red") +
+  geom_hline(data = activas %>% group_by(provincia) %>% summarise(ymin = quantile(liquidez_corriente, 0.25), ymed = median(liquidez_corriente), ymax = quantile(liquidez_corriente, 0.75)),
+             aes(yintercept = ymed), linetype = "dashed", color = "purple") +
+  geom_hline(data = activas %>% group_by(provincia) %>% summarise(ymin = quantile(liquidez_corriente, 0.25), ymed = median(liquidez_corriente), ymax = quantile(liquidez_corriente, 0.75)),
+             aes(yintercept = ymax), linetype = "dashed", color = "black") +
+  labs(title = "Comparativo de Indicadores de Liquidez por situacion ACTIVA y provincia",
+       x = "Fecha constitución",
+       y = "Liquidez") +
   facet_wrap(~provincia)+
+  theme_minimal()+
   theme(legend.position = "none")
+
+
+activas$apalancamiento<-impute_outliers(activas$apalancamiento)
+plot(activas$apalancamiento)
+ggplot(activas, aes(x=fecha_const, y=apalancamiento, color = provincia, group=provincia))   +
+  geom_line() +
+  geom_hline(data = activas %>% group_by(provincia) %>% summarise(ymin = quantile(apalancamiento, 0.25), ymed = median(apalancamiento), ymax = quantile(apalancamiento, 0.75)),
+             aes(yintercept = ymin), linetype = "dashed", color = "red") +
+  geom_hline(data = activas %>% group_by(provincia) %>% summarise(ymin = quantile(apalancamiento, 0.25), ymed = median(apalancamiento), ymax = quantile(apalancamiento, 0.75)),
+             aes(yintercept = ymed), linetype = "dashed", color = "purple") +
+  geom_hline(data = activas %>% group_by(provincia) %>% summarise(ymin = quantile(apalancamiento, 0.25), ymed = median(apalancamiento), ymax = quantile(apalancamiento, 0.75)),
+             aes(yintercept = ymax), linetype = "dashed", color = "black") +
+  labs(title = "Comparativo de Indicadores de Apalancamiento por situacion ACTIVA y provincia",
+       x = "Fecha constitución",
+       y = "Liquidez") +
+  facet_wrap(~provincia)+
+  theme_minimal()+
+  theme(legend.position = "none")
+
+#Gráficamente muestra el comparativo de los indicadores financieros de liquidez y
+#solvencia por tipo de empresa.
+
+ggplot(activas, aes(x=fecha_const, y=liquidez_corriente, color = tipo, group=tipo))   +
+  geom_line() +
+  geom_hline(data = activas %>% group_by(tipo) %>% summarise(ymin = quantile(liquidez_corriente, 0.25), ymed = median(liquidez_corriente), ymax = quantile(liquidez_corriente, 0.75)),
+             aes(yintercept = ymin), linetype = "dashed", color = "red") +
+  geom_hline(data = activas %>% group_by(tipo) %>% summarise(ymin = quantile(liquidez_corriente, 0.25), ymed = median(liquidez_corriente), ymax = quantile(liquidez_corriente, 0.75)),
+             aes(yintercept = ymed), linetype = "dashed", color = "purple") +
+  geom_hline(data = activas %>% group_by(tipo) %>% summarise(ymin = quantile(liquidez_corriente, 0.25), ymed = median(liquidez_corriente), ymax = quantile(liquidez_corriente, 0.75)),
+             aes(yintercept = ymax), linetype = "dashed", color = "black") +
+  labs(title = "Comparativo de Indicadores de Liquidez por situacion ACTIVA y tipo",
+       x = "Fecha constitución",
+       y = "Liquidez") +
+  facet_wrap(~tipo)+
+  theme_minimal()+
+  theme(legend.position = "none")
+
+
+
+ggplot(activas, aes(x=fecha_const, y=apalancamiento, color = tipo, group=tipo))   +
+  geom_line() +
+  geom_hline(data = activas %>% group_by(tipo) %>% summarise(ymin = quantile(apalancamiento, 0.25), ymed = median(apalancamiento), ymax = quantile(apalancamiento, 0.75)),
+             aes(yintercept = ymin), linetype = "dashed", color = "red") +
+  geom_hline(data = activas %>% group_by(tipo) %>% summarise(ymin = quantile(apalancamiento, 0.25), ymed = median(apalancamiento), ymax = quantile(apalancamiento, 0.75)),
+             aes(yintercept = ymed), linetype = "dashed", color = "purple") +
+  geom_hline(data = activas %>% group_by(tipo) %>% summarise(ymin = quantile(apalancamiento, 0.25), ymed = median(apalancamiento), ymax = quantile(apalancamiento, 0.75)),
+             aes(yintercept = ymax), linetype = "dashed", color = "black") +
+  labs(title = "Comparativo de Indicadores de Apalancamiento por situacion ACTIVA y tipo",
+       x = "Fecha constitución",
+       y = "Liquidez") +
+  facet_wrap(~tipo)+
+  theme_minimal()+
+  theme(legend.position = "none")
+
+#¿El endeudamiento del activo fue mayor en empresas micro + pequeñas vs. grandes?
+
+micro_pequeña <- balance %>%
+  filter(tamanio%in% c("MICRO", "PEQUEÑA")) %>%
+  summarise(Promedio1 = mean(endeu_activo))
+micro_pequeña
+
+grande <- balance %>%
+  filter(tamanio%in% c("GRANDE")) %>%
+  summarise(Promedio2 = mean(endeu_activo))
+grande
+
+comparativo<- tibble(Tamaño=c("Micro+Pequeña","Grande"),
+                     Media=c(micro_pequeña$Promedio1,grande$Promedio2)) %>% view()
+
+
+ggplot(comparativo, aes(x = Tamaño, y = Media, fill = Tamaño)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Comparación de Endeudamiento del Activo entre Micro + Pequeña y Grande",
+       x = "Tamaño de Empresa", y = "Valor de Endeudamiento del Activo") +
+  theme_minimal()
+
+#¿La liquidez por tipo de compañía es diferente entre aquellas empresas que tienen más de
+#60 trabajadores directos y que cuenta con 100 a 800 trabajadores administrativos?
+
+levels(as.factor(balance$tipo))
+liquidez_compania<- balance %>% select(tipo,trab_direc,trab_admin,liquidez_corriente) %>% 
+  filter(trab_direc>60,trab_admin>=100,trab_admin<=800) 
+
+liquidez_compania %>% group_by(tipo)%>% 
+  summarise(Promedio3 = mean(liquidez_corriente))
+
+ggplot(liquidez_compania, aes(x = tipo, y = liquidez_corriente, fill = tipo)) +
+  geom_jitter(alpha=1,color="gray")+
+  geom_boxplot(alpha=0.1) +
+  labs(title = "Comparación de Liquidez por Tipo de Compañía",
+       x = "Tipo de Compañía", y = "Liquidez") +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+#Describe el top 10 de empresas con mayor apalancamiento.
+balance %>% select(nombre_cia,apalancamiento) %>% 
+  arrange(desc(apalancamiento)) %>% 
+  head(10)
